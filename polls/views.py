0909bb57dd378ea828013212509398f058d2e105
Amit Singh <amit8564880158@gmail.com>
2019-04-10 21:06:28 +0530

    added base.html

--- polls/views.py
+++ polls/views.py
@@ -5,15 +5,12 @@ from .models import Question
 
 def index(request):
 	latest_questions= Question.objects.order_by('-pub_date')[:5]
-	#template = loader.get_template('polls/index.html')
-	#context=RequestContext(request, {'latest_questions': latest_questions})
-	#return HttpResponse(template.render(context.flatten()))
 	return render(request,'polls/index.html',{'latest_questions':latest_questions})
 
 
 def detail(request, question_id):
-	detail = 'This is the detail of the question: '+question_id
-	return HttpResponse(detail)
+	question=Question.objects.get(pk=question_id)
+	return render(request, 'polls/detail.html',{'question':question})
 
 def result(request, question_id):
 	result = 'This is the result of the question: '+question_id
